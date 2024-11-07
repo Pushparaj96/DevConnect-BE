388d1b3086c662ed33ac6155ae12cdb5e9ead28f
Pushparaj96 <kpushparaj18@gmail.com>
2024-11-07 16:31:56 +0530

    multiple route Handlers for one route

--- src/app.js
+++ src/app.js
@@ -2,13 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send("welcome PK!")
-})
-
-app.use("/",(req,res)=>{
-    res.send("Pushparaj");
-})
+app.get("/user",(req,res,next)=>{
+    console.log("1st route Handler!");
+    next();
+    //res.send("Response from 1st route handler");
+    
+},(req,res,next)=>{
+    console.log("2nd route Handler!");
+    //res.send("Response from 2nd Route handler");
+    next();
+},
+[
+(req,res,next)=>
+    {
+    console.log("3rd route Handler!");
+    next();
+},(req,res,next)=>
+    {
+    console.log("4th route Handler");
+    res.send("Response from 4th route handler") ;
+}]
+)
 
 app.listen(3000,()=>{
     console.log("Server started at port 3000");
