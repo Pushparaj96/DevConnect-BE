41092f460359bc2794c764e73f4797733a7a5185
Pushparaj96 <kpushparaj18@gmail.com>
2024-11-07 18:32:24 +0530

    testing middleware

--- src/app.js
+++ src/app.js
@@ -2,27 +2,45 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user",(req,res,next)=>{
-    console.log("1st route Handler!");
-    next();
-    //res.send("Response from 1st route handler");
-    
-},(req,res,next)=>{
-    console.log("2nd route Handler!");
-    //res.send("Response from 2nd Route handler");
-    next();
-},
-[
-(req,res,next)=>
-    {
-    console.log("3rd route Handler!");
-    next();
-},(req,res,next)=>
-    {
-    console.log("4th route Handler");
-    res.send("Response from 4th route handler") ;
-}]
-)
+app.use("/admin",(req,res,next)=>{
+    const token = "abc";
+    const isAuthorized = (token === "abc");
+    if(!isAuthorized){
+        res.status(401).send("Unauthorized Request");
+    }else{
+        console.log("Authorized Admin!");
+        next();
+    }
+});
+
+app.post("/user/login",(req,res)=>{
+    res.send("No auth needed");
+})
+
+app.use("/user",(req,res,next)=>{
+    const token = "xyz";
+    const isAuthorized = (token === "xyz");
+    if(!isAuthorized){
+        res.status(401).send("Unauthorized request by You !");
+    }else{
+        console.log("Authorized user !");
+        next();
+    }
+})
+
+
+
+app.get("/user/dashboard",(req,res)=>{
+    res.send("Fetched Dashboard Successfully");
+})
+
+app.get("/admin/allusers",(req,res)=>{
+    res.send("Fetched all user details");
+});
+
+app.get("/admin/panel",(req,res)=>{
+    res.send("Fetched admin panel");
+})
 
 app.listen(3000,()=>{
     console.log("Server started at port 3000");
