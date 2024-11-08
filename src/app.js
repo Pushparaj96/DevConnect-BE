12ab6e6d82ee63908e403375a90d358220992bb8
Pushparaj96 <kpushparaj18@gmail.com>
2024-11-08 18:08:39 +0530

    saved first  user Document in DB

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 
 
 
 const app = express();
+
+app.post("/signup", async (req,res)=>{
+
+    const user = new User({ // instance of User model
+        firstName:"John",
+        lastName:"Joe",
+        emailId:"john@gmail.com",
+        password:"john@123",
+        age:24,
+        gender:"Male"
+    });
+
+    try{
+        await user.save(); // saving document into DB
+        res.send("Signup Successful");
+    }catch(err){
+        res.status(400).send("Failed to Signup");
+    }
+})
 
 
 connectDB()
