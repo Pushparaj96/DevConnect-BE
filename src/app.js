10394b6d64426860b003055ef5eb7615b19319f8
Pushparaj96 <kpushparaj18@gmail.com>
2024-11-08 23:07:16 +0530

    get users API

--- src/app.js
+++ src/app.js
@@ -6,6 +6,40 @@ const app = express();
 
 app.use(express.json()) ; // express.json() middleware reads JSON and convert it into js Object on every Route
 
+app.get("/feed", async (req,res)=>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(err){
+        res.status(401).send("Something went Wrong");
+    }
+});
+
+app.get("/byId", async (req,res)=>{
+
+    try {
+        const user = await User.findById(req.body._id);
+            res.send(user);
+        
+    } catch (error) {
+        res.status(401).send("Something Went Wrong");
+    }
+        
+})
+
+app.get("/user", async(req,res)=>{
+    try {
+        const users = await User.find({emailId:req.body.emailId});
+        if(!users.length){
+            res.status(404).send("User not Found");
+        }else{
+            res.send(users);
+        }    
+    } catch (error) {
+            res.status(400).send("something went Wrong");
+    }
+})
+
 app.post("/signup", async (req,res)=>{
 
     // instance of User model
