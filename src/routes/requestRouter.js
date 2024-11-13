2ad22906c84ea50563c932168c4c52b89a4d668d
Pushparaj96 <kpushparaj18@gmail.com>
2024-11-13 22:30:30 +0530

    fix:search query

--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -24,9 +24,9 @@ requestRouter.post("/request/send/:status/:receiverId",userAuth, async (req,res)
             return res.status(404).json({message:`User Not Found - can't Send Request!`})
         }
 
-        const isExistingRequest = await ConnectionRequest.find({$or:[{senderId,receiverId,status:"interested"},{senderId:receiverId,receiverId:senderId,status:"interested"}]});
+        const isExistingRequest = await ConnectionRequest.findOne({$or:[{senderId,receiverId,status:"interested"},{senderId:receiverId,receiverId:senderId,status:"interested"}]});
 
-        if(isExistingRequest.length >= 1){
+        if(isExistingRequest){
             return res.status(400).json({message:`This Request is Already There ! check Your pending Requests...`});
         }
 
