fdda3b18e48500c5fb4ddd4d00bbbd3ed7b81d4d
Pushparaj96 <kpushparaj18@gmail.com>
2024-11-13 22:52:02 +0530

    refactoring

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -24,8 +24,8 @@ connectionRequestSchema.index({senderId:1,receiverId:1});
 
 // pre middeleware checks before every save in DB 
 connectionRequestSchema.pre("save",function(next){
-   const ConnectionRequest = this;
-   if(ConnectionRequest.senderId.equals(this.receiverId)){
+   const ConnectionRequest = this; // Short hand { senderId , receiverId } = this;
+   if(ConnectionRequest.senderId.equals(ConnectionRequest.receiverId)){
     throw new Error ("You can't Send Request to Yourself!");
    }
    next(); // move to route handler
